fb454fcc3cc7e99915837f8ebba4e0bbb1d67c70
Teayyyy <zgsighoty@outlook.com>
2023-05-28 00:38:50 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+"""
+This file is used for the main function of the program
+which is face recognition, to identify whether the person belongs to the group
+Data are set as follows:
+  Each person, contains several images of its faces, and is preprocessed to face-descriptors (128D)
+  Data are stored in a .cvs file, and will be loaded when program initialize.
+
+* new person: see create_new.py
+"""
+
+
+
